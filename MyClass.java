94e7cecef272a1076788162d3bcbc59715825e61
zy2611 <zma9244@students.uc-bcf.edu.ph>
2024-08-01 09:11:51 +0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    int x=10;
+    int y=25;
+    int z=x+y;
+
+    System.out.println("Sum of x+y = " + z);
+  }
+}
